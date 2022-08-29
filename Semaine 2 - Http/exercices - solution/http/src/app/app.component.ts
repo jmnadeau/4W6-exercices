750827f5b6a30788259b92b8320fe0cee3bbe8a0
Jim <Jim@jim.jim>
2022-08-29 11:14:01 -0400

    Ajout à l'exercice de la semaine 2

--- Semaine 2 - Http/exercices - solution/http/src/app/app.component.ts
+++ Semaine 2 - Http/exercices - solution/http/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { lastValueFrom } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -14,8 +15,13 @@ export class AppComponent implements OnInit {
   constructor(public http: HttpClient) { }
 
   ngOnInit(): void {
+    this.requeteAvecSubscribe();
+    this.requeteAvecAsyncAwait();
+  }
+  
+  requeteAvecSubscribe() {
     let URL='http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&api_key=9a8a3facebbccaf363bb9fd68fa37abf&format=json&artist=' + this.artist;
-    this.http.get<any>(URL).subscribe(res => {
+    this.http.get<any>(URL).subscribe((res:any) => {
 
       console.log(res);
 
@@ -36,6 +42,30 @@ export class AppComponent implements OnInit {
         this.similarArtists.push(similar.name);
       });
     });
+  }
+
+  // On ajoute async devant la méthode pour pouvoir utiliser await à l'intérieur de la méthode 
+  async requeteAvecAsyncAwait() {
+    let URL='http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&api_key=9a8a3facebbccaf363bb9fd68fa37abf&format=json&artist=' + this.artist;
+    
+    // await permet à la méthode de se mettre en pause pour attendre la fin de l'exécution de la requête HTTP
+    let res:any = await lastValueFrom(this.http.get<any>(URL))
+
+    console.log(res);
+
+    let lastFmSimilars = res.artist.similar.artist;
+
+    for(let i=0; i<lastFmSimilars.length; i++) {
+      this.similarArtists.push(lastFmSimilars[i].name);
+    }
+
+    this.similarArtists = [];
+
+      // On pourrait aussi utiliser un foreach
+    lastFmSimilars.forEach((similar: any) => {
+      this.similarArtists.push(similar.name);
+    });
+
   }
 }
 
